4e7640740aba4d9b8296d335cdb4e75c82e761cd
kgiann78 <kgiann78@gmail.com>
2015-11-02 23:27:00 +0200

    remove old visitor example

--- Visitor/StudyCaseInterface.h
+++
@@ -1,9 +0,0 @@
-#ifndef STUDY_CASE_VIS_INT
-#define STUDY_CASE_VIS_INT
-#include "StudyCaseVisitorInterface.h"
-#endif
-class StudyCaseInterface
-{
-public:
-	virtual void access(StudyCaseVisitorInterface *visitorInterface) = 0;
-};
--- Visitor/StudyCaseVisitorInterface.h
+++
@@ -1,5 +0,0 @@
-class StudyCaseVisitorInterface
-{
-public:
-	virtual void visit(class Class1 *class1) = 0;
-};
